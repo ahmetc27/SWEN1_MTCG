e71cf2b7a6a41f503354885a1a658bdfe0f5f207
Cicek <ahmetcicek085@gmail.com>
2024-10-13 19:25:24 +0200

    Implement further features in User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -11,20 +11,36 @@ namespace MTCG
         public string Username { get; set; }
         public string Password { get; set; }
 
-        List<Card> Stack { get; set; } = new List<Card>();
+        public List<Card> Stack { get; set; } = new List<Card>();
 
-        List<Card> Deck { get; set; } = new List<Card>();
+        public List<Card> Deck { get; set; } = new List<Card>();
 
         public int Coins { get; set; } = 20;
 
         public int ELO { get; set; } = 100;
 
-        public int GamesPlayed { get; set; }
+        public int GamesPlayed { get; set; } = 0;
 
         public User(string username, string password)
         {
             Username = username;
             Password = password;
+        }
+        public void AddCardToStack(Card card)
+        {
+            Stack.Add(card);
+        }
+
+        public void AddCardToDeck(Card card)
+        {
+            if (Deck.Count < 4) // Annahme: Deck darf maximal 4 Karten haben
+            {
+                Deck.Add(card);
+            }
+            else
+            {
+                Console.WriteLine("Deck ist already full!");
+            }
         }
     }
 }
