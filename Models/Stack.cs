7f7830f4348819ba3442d0b8b8149e997a82c126
Cicek <ahmetcicek085@gmail.com>
2024-10-13 19:16:02 +0200

    Implement further features in Stack.cs

--- Models/Stack.cs
+++ Models/Stack.cs
@@ -21,7 +21,40 @@ namespace MTCG
         }
         public void RemoveCard(Card card)
         {
-            StackCards.Remove(card);
+            if (StackCards.Contains(card))
+            {
+                StackCards.Remove(card);
+            }
+            else
+            {
+                Console.WriteLine("Card not found in the stack!");
+            }
+        }
+
+        public Card DrawCard()
+        {
+            if (StackCards.Count == 0)
+            {
+                Console.WriteLine("No cards in the stack!");
+                return null;
+            }
+            Card drawnCard = StackCards[0];
+            StackCards.RemoveAt(0);  // Entfernt die oberste Karte
+            return drawnCard;
+        }
+
+        public Card DrawRandomCard()
+        {
+            if (StackCards.Count == 0)
+            {
+                Console.WriteLine("No cards in the stack!");
+                return null;
+            }
+            Random rng = new Random();
+            int index = rng.Next(StackCards.Count);
+            Card drawnCard = StackCards[index];
+            StackCards.RemoveAt(index);
+            return drawnCard;
         }
     }
 }
